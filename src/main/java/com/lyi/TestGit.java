b60407f390b62e8828287c6617e785ac3404eefa
lyi <lyi416@163.com>
2024-05-06 17:02:32 +0800

    master test commit

--- src/main/java/com/lyi/TestGit.java
+++ src/main/java/com/lyi/TestGit.java
@@ -19,5 +19,6 @@ public class TestGit {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("master test");
     }
 }
