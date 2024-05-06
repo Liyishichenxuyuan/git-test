0385f36aedc29f50b7d6e415764fd2f55e54d3c4
lyi <lyi416@163.com>
2024-05-06 15:46:32 +0800

    first commit

---
+++ src/main/java/com/lyi/TestGit.java
@@ -0,0 +1,20 @@
+package com.lyi;
+
+/**
+ * className: TestJavaProcess
+ * <p>
+ * description: 测试 IDEA 集成 GIT
+ * <p>
+ * author: Yi
+ * <p>
+ * date: 2024/4/12 15:13
+ * <p>
+ * version: v1.0
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+    }
+}
