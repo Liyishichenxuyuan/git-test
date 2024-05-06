05b779e32a0a1c1c47774bc5259d534a4dd78efb
lyi <lyi416@163.com>
2024-05-06 16:54:39 +0800

    hot-fix commit

--- src/main/java/com/lyi/TestGit.java
+++ src/main/java/com/lyi/TestGit.java
@@ -18,5 +18,6 @@ public class TestGit {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
